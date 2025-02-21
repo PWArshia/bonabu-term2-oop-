4fc20fe9f7bf4b78c502dfcf4b9356c28edecba9
arshia <arshia.sedghi011@gmail.com>
2025-02-22 01:05:04 +0330

    change getter && add some function

--- gBigNamCalc/src/BigInt.java
+++ gBigNamCalc/src/BigInt.java
@@ -12,6 +12,7 @@ public class BigInt {
     }
 
     public BigInt(String s){
+        s=checker(s);
         A=new int [s.length()];
         cA=0;
         if (s.charAt(0)=='+' || s.charAt(0)=='-') {
@@ -19,6 +20,9 @@ public class BigInt {
             for (int i=1; i<s.length(); i++) {
                 A[i-1]=s.charAt(i)-'0';
                 cA=i;
+            }
+            if (sign=='-') {
+                A[0]=-A[0];
             }
         }
         else{
@@ -40,6 +44,9 @@ public class BigInt {
                 A[i-1]=s.charAt(i)-'0';
                 cA=i;
             }
+            if (sign=='-') {
+                A[0]=-A[0];
+            }
         }
         else{
             sign='+';
@@ -54,6 +61,7 @@ public class BigInt {
     //   --------------------------------------> SETTERS
 
     public void Set(String s){
+        s=checker(s);
         A=new int [s.length()];
         cA=0;
         if (s.charAt(0)=='+' || s.charAt(0)=='-') {
@@ -61,6 +69,9 @@ public class BigInt {
             for (int i=1; i<s.length(); i++) {
                 A[i-1]=s.charAt(i)-'0';
                 cA=i;
+            }
+            if (sign=='-') {
+                A[0]=-A[0];
             }
         }
         else{
@@ -85,20 +96,132 @@ public class BigInt {
     //   --------------------------------------> GETTER
 
     public int [] Get(){
-        return A;
+        return this.A;
     }
 
-//    public String GetString(){
-//        String s="";
-//        for (int i=0; i<A.length; i++) {
-//            s+=A[i];
-//        }
-//        return s;
-//    }
+    public String GetString(){
+        String s="";
+        for (int i=0; i<A.length; i++) {
+            s+=A[i];
+        }
+        return s;
+    }
+
+    public int length(){
+        return this.cA+1;
+    }
 
     //   --------------------------------------> FUNCTIONS
 
+    private String checker( String s1){
 
+        String s2="";
+
+        if (s1.charAt(0)=='+' || s1.charAt(0)=='-' || s1.charAt(0)>='0' || s1.charAt(0)<='9') {
+            s2+=s1.charAt(0);
+            for (int i=1; i<s1.length(); i++) {
+                s2+=s1.charAt(i);
+            }
+        }
+
+        else{
+            s2="0";
+        }
+
+        return s2;
+    }
+
+    private void reverse(){
+
+
+
+    }
+
+    public int compare(BigInt x){
+        if(this.sign==x.sign){
+            if(this.sign=='+'){
+                if(this.A.length>x.A.length){
+                    return 1;
+                }
+                else if (this.A.length<x.A.length){
+                    return -1;
+                }
+                else {
+                    for(int i=0; i<this.A.length; i++){
+                        if(this.A[i]>x.A[i]){
+                            return 1;
+                        }
+                        else if(this.A[i]<x.A[i]){
+                            return -1;
+                        }
+                    }
+                    return 0;
+                }
+            }
+            else {
+                if (this.A.length > x.A.length) {
+                    return -1;
+                } else if (this.A.length < x.A.length) {
+                    return +1;
+                } else {
+                    for (int i = 0; i < this.A.length; i++) {
+                        if (this.A[i] > x.A[i]) {
+                            return 1;
+                        } else if (this.A[i] < x.A[i]) {
+                            return -1;
+                        }
+                    }
+                    return 0;
+                }
+            }
+        }
+        else{
+            if(this.sign=='-'){
+                return -1;
+            }
+            else if(this.sign=='+'){
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public int compare(int x){
+        BigInt y=new BigInt(x);
+        return this.compare(y);
+    }
+
+
+
+    public void SetSum( BigInt b){
+
+        int min=this.A.length;
+        if(min>b.A.length){
+            min=b.A.length;
+        }
+
+
+        if (this.sign==b.sign){
+            if (this.sign=='+'){
+                int q=0;
+                for (int i=0; i<min; i++){
+                    this.A[i]+=b.A[i]+q;
+                    q=0;
+                    if (this.A[i]>9){
+                        this.A[i]-=10;
+                        q++;
+                    }
+                }
+            }
+            else{
+                int q=0;
+            }
+
+
+
+
+        }
+    }
 
 
 
--- gBigNamCalc/src/Main.java
+++ gBigNamCalc/src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        BigInt A = new BigInt(144);
+        BigInt B = new BigInt(2);
+        A.SetSum(B);
+        for (int x=0; x< A.length();x++){
+            System.out.print(A.Get()[x]);
+        }
     }
 }
