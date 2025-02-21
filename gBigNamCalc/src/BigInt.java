78c9101e467196a65b2af0dd62545be872acf4f2
arshia <arshia.sedghi011@gmail.com>
2025-02-21 20:59:05 +0330

    edit constructors

--- gBigNamCalc/src/BigInt.java
+++ gBigNamCalc/src/BigInt.java
@@ -1,18 +1,53 @@
 public class BigInt {
 
-    private String num;
+    private int A[];
+    private int cA;
+    private char sign;
 
     //   --------------------------------------> CONSTRUCTORS
 
     public BigInt() {
-        this.num ="0";
-    }
-    public BigInt(String s1) {
-        this.num = s1;
+        A= new int[100000];
+        cA=0;
     }
 
-    public BigInt(int x){
-        this.num = String.valueOf(x);
+    public BigInt(String s){
+        A=new int [s.length()];
+        cA=0;
+        if (s.charAt(0)=='+' || s.charAt(0)=='-') {
+            sign=s.charAt(0);
+            for (int i=1; i<s.length(); i++) {
+                A[i-1]=s.charAt(i);
+                cA=i;
+            }
+        }
+        else{
+            sign='+';
+            for (int i=0; i<s.length(); i++) {
+                A[i]=s.charAt(i);
+                cA=i;
+            }
+        }
+    }
+
+    public BigInt(long x){
+        String s = Long.toString(x);
+        A=new int [s.length()];
+        cA=0;
+        if (s.charAt(0)=='+' || s.charAt(0)=='-') {
+            sign=s.charAt(0);
+            for (int i=1; i<s.length(); i++) {
+                A[i-1]=s.charAt(i);
+                cA=i;
+            }
+        }
+        else{
+            sign='+';
+            for (int i=0; i<s.length(); i++) {
+                A[i]=s.charAt(i);
+                cA=i;
+            }
+        }
     }
 
 
