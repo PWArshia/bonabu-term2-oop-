5cc4cee36fdbe4e92438c980e82990e433a4d11f
arshia <arshia.sedghi011@gmail.com>
2025-02-21 18:47:51 +0330

    add constructors

--- gBigNamCalc/src/BigInt.java
+++ gBigNamCalc/src/BigInt.java
@@ -4,9 +4,16 @@ public class BigInt {
 
     //   --------------------------------------> CONSTRUCTORS
 
+    public BigInt() {
+        this.num ="0";
+    }
+    public BigInt(String s1) {
+        this.num = s1;
+    }
 
-
-
+    public BigInt(int x){
+        this.num = String.valueOf(x);
+    }
 
 
     //   --------------------------------------> SETTERS
